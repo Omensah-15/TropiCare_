0a9cdf68e4dc004a63fd362f03af6f0f9bb5c8b5
Mensah Obed <heavenzlebron7@gmail.com>
2026-06-29 09:24:23 +0000

    Add optimization and build options to Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['jspdf'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/jspdf/, /node_modules/],
+    },
+  },
 })
